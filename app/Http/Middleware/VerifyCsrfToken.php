4f0204a267d63f4b84b5c2347480d5f6550eda20
Nauman Ahmad <naumanahmed19@hotmail.com>
2017-10-18 13:42:43 +0500

    CSRF Protected Update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        "http://xvelopers.com/animus/",
+        "http://xvelopers.com/"
     ];
 }
