4e052ae35f3c7a0f49af3dfa0dc26f3c42e6a39c
Nauman Ahmad <naumanahmed19@hotmail.com>
2017-10-18 16:00:17 +0500

    Mail Template Updated

--- resources/views/emails/apartment.blade.php
+++ resources/views/emails/apartment.blade.php
@@ -1,24 +1,26 @@
+{{--@formatter:off--}}
 @component('mail::message')
-    # Apartment Posted
+# Apartment Posted
 
-    Your new apartment has been saved and publish. Here are details.
+Your new apartment has been saved and publish. Here are details.
 
-    Apartment Id: {{$apartment->id}}
+Apartment Id: {{$apartment->id}}
 
-    Apartment Title: {{$apartment->title}}
+Apartment Title: {{$apartment->title}}
 
-    @php
-        $editUrl =   config('app.frontendUrl').'apartments/'.$apartment->id.'/edit/'.$apartment->token;
-        $delUrl =  config('app.frontendUrl').'apartments/'.$apartment->id.'/delete/'.$apartment->token;
-    @endphp
+@php
+$editUrl =   config('app.frontendUrl').'apartments/'.$apartment->id.'/edit/'.$apartment->token;
+$delUrl =  config('app.frontendUrl').'apartments/'.$apartment->id.'/delete/'.$apartment->token;
+@endphp
 
-    @component('mail::button', ['url' => $editUrl])
-        Edit Apartment
-    @endcomponent
-
-    Want to delete this apartment? click following url?
-    <a href="{{$delUrl}}">{{$delUrl}}</a>
-
-    Thanks,<br>
-    {{ config('app.name') }}
+@component('mail::button', ['url' => $editUrl])
+Edit Apartment
 @endcomponent
+
+Want to delete this apartment? click following url?
+<a href="{{$delUrl}}">{{$delUrl}}</a>
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
+{{--@formatter:on--}}
