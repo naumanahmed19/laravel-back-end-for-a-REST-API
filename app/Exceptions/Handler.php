c4fce1cd14cfb607980ec79830b528d87c3ea508
Nauman Ahmad <naumanahmed19@hotmail.com>
2017-11-27 03:16:33 +0500

    phpDock block and readme

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-
+        return $request->all();
         return parent::render($request, $exception);
     }
 }
